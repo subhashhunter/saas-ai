c166d2f9febe08d82fa9b825ea40675c44da1de5
subhashhunter <subhash56190@gmail.com>
2025-03-17 00:56:04 +0530

    add docker file

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -25,7 +25,7 @@ export async function GET(req:NextRequest){
         })
         const totalItems=await prisma.todo.count({
             where:{
-                id:userId,
+                userId,
                 title:{
                     contains:"search",
                     mode:"insensitive"
